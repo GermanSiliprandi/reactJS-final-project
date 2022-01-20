470243c7905900def2749a56b1b81e30c14938e6
German Siliprandi <german.siliprandi@gmail.com>
2022-01-19 22:55:18 -0300

    Desafio 5

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { ItemListContainer } from "./components/ItemListContainer/ItemListContainer";
 import { NavBar } from "./components/NavBar/NavBar";
+import { ItemCount } from "./components/Counter/ItemCount";
 function App() {
 	return (
 		<div className="App">
@@ -8,7 +9,13 @@ function App() {
 				<NavBar />
 			</header>
 			<main>
-				<ItemListContainer text="PRODUCT CONTAINER 1" />
+				<ItemListContainer
+					id={1}
+					name="Monitor"
+					price={7500}
+					description={`El Monitor mas moderno del momento`}
+					stock={10}
+				></ItemListContainer>
 			</main>
 		</div>
 	);
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,8 +1,49 @@
 import React from "react";
-export const ItemListContainer = ({ text }) => {
+export const ItemListContainer = ({ id, name, price, description, stock }) => {
+	const Add = ({ state }) => {
+		console.log(`${state} Items Added`);
+	};
+	const ItemCount = ({ stock, initial, onAdd }) => {
+		const [counter, setCounter] = React.useState(initial);
+
+		React.useEffect((initial) => {
+			setCounter(initial);
+			return () => {
+				setCounter(initial);
+			};
+		}, []);
+		const inc = () => {
+			if ({ stock } >= counter) {
+				let increase = counter;
+				increase++;
+				setCounter(increase);
+			}
+		};
+		const dec = () => {
+			if (counter >= 0) {
+				let decrease = counter;
+				decrease--;
+				setCounter(decrease);
+			}
+		};
+
+		return (
+			<div>
+				<button onClick={inc}>+</button>
+				<span>{counter}</span>
+				<button onClick={dec}>-</button>
+				<button onClick={() => onAdd(counter)}>Add to Cart</button>
+			</div>
+		);
+	};
 	return (
-		<>
-			<p>{text}</p>
-		</>
+		<div>
+			<p>id:{id} </p>
+			<p>name:{name}</p>
+			<p>price:{price}</p>
+			<p>description:{description} </p>
+			<p>stock: {stock} </p>
+			<ItemCount initial={1} />
+		</div>
 	);
 };
